3a1b1ff1a68f85a01d691ffad17744b7f7f5a974
bridnya.v.v <bridnya.v.v@sbertech.ru>
2026-04-18 17:02:30 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,48 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type WordCount struct {
+	Word  string
+	Count int
+}
+
+func Top10(text string) []string {
+	textSlice := strings.Fields(text)
+	counts := make(map[string]int)
+
+	for _, word := range textSlice {
+		counts[word] = counts[word] + 1 //nolint
+	}
+
+	keys := make([]string, 0, len(counts))
+	for k := range counts {
+		keys = append(keys, k)
+	}
+	wordCounts := make([]WordCount, 0, len(keys))
+	for _, word := range keys {
+		wordCounts = append(wordCounts, WordCount{word, counts[word]})
+	}
+	// Сортировка слайса по алфавиту и частоте
+	sort.SliceStable(wordCounts, func(i, j int) bool {
+		if wordCounts[i].Count == wordCounts[j].Count {
+			// Если частоты равны, сортируем по слову (лексикографически)
+			return wordCounts[i].Word < wordCounts[j].Word
+		}
+		// Если частоты разные, сортируем по частоте (по убыванию)
+		return wordCounts[i].Count > wordCounts[j].Count
+	})
+	topN := 10
+	if len(wordCounts) == 0 {
+		return nil
+	}
+	resultStruct := wordCounts[:topN]
+	res := make([]string, 0)
+	for _, v := range resultStruct {
+		res = append(res, v.Word)
+	}
+	return res
 }
