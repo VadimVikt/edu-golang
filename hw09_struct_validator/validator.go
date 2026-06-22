7da7539be95ce0d3d15be61affbda33aa49a3f36
bridnya.v.v <bridnya.v.v@sbertech.ru>
2026-06-22 23:26:58 +0300

    HW9 completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,36 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"reflect"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const (
+	TagLength  = "len"
+	TagMin     = "min"
+	TagMax     = "max"
+	TagIn      = "in"
+	TagRequire = "required"
+	TagRegex   = "regexp"
+)
+
+var (
+	ErrInvalidRegexp   = errors.New("invalid regexp")
+	ErrRequiredField   = errors.New("field required")
+	ErrInvalidLength   = errors.New("invalid length")
+	ErrValueOutOfRange = errors.New("value out of range")
+	ErrInvalidFormat   = errors.New("invalid format")
+	ErrValueNotInList  = errors.New("value not in allowed list")
+	ErrTagNotProvided  = errors.New("the tag is not provided by the validator")
+	ErrLessThanMin     = errors.New("field is less than min")
+	ErrGraterThanMax   = errors.New("field is greater than max")
+	ErrExpectedStruct  = errors.New("expected a struct")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +39,195 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	if len(v) == 0 {
+		return "no errors"
+	}
+	errorMessages := make([]string, 0, len(v))
+	for _, err := range v {
+		errorMessages = append(errorMessages, err.Err.Error())
+	}
+	return strings.Join(errorMessages, "\n ")
+}
+
+func (v ValidationErrors) Is(target error) bool {
+	for _, err := range v {
+		if errors.Is(err.Err, target) {
+			return true
+		}
+	}
+	return false
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
-	return nil
+	var result ValidationErrors
+	t := reflect.TypeOf(v)
+
+	if t.Kind() != reflect.Struct {
+		return ErrExpectedStruct
+	}
+	val := reflect.ValueOf(v)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tagString := field.Tag.Get("validate")
+		if tagString == "" {
+			continue
+		}
+		tags := parseTags(tagString)
+		switch field.Type.Kind() { //nolint
+		case reflect.String:
+			err := validateString(field.Name, val.Field(i).String(), tags)
+			if err.Err == nil {
+				continue
+			}
+			result = append(result, err)
+		case reflect.Int:
+			err := validateInt(field.Name, int(val.Field(i).Int()), tags)
+			if err.Err == nil {
+				continue
+			}
+			result = append(result, err)
+		case reflect.Slice:
+			switch kind := field.Type.Elem().Kind(); kind { //nolint
+			case reflect.String:
+				err := validateSliceString(field.Name, val.Field(i), tags)
+				if err.Err == nil {
+					continue
+				}
+				result = append(result, err)
+			case reflect.Int:
+				err := validateSliceInt(field.Name, val.Field(i), tags)
+				if err.Err == nil {
+					continue
+				}
+				result = append(result, err)
+			default:
+				continue
+			}
+		default:
+			continue
+		}
+	}
+	return result
+}
+
+func validateSliceString(fieldName string, field reflect.Value, tags map[string]string) ValidationError {
+	for k, v := range tags {
+		switch k {
+		case TagLength:
+			lenField, err := strconv.Atoi(v)
+			if err != nil {
+				return ValidationError{fieldName, ErrTagNotProvided}
+			}
+			tmp := field.Interface().([]string)
+			for _, str := range tmp {
+				if len(str) != lenField {
+					return ValidationError{fieldName, ErrInvalidLength}
+				}
+			}
+		default:
+			return ValidationError{fieldName, ErrTagNotProvided}
+		}
+	}
+	return ValidationError{fieldName, nil}
+}
+
+func validateSliceInt(fieldName string, field reflect.Value, tags map[string]string) ValidationError {
+	for k, v := range tags {
+		switch k {
+		case TagLength:
+			lenField, _ := strconv.Atoi(v)
+			if len(field.Interface().([]int)) != lenField {
+				return ValidationError{fieldName, ErrInvalidLength}
+			}
+		default:
+			return ValidationError{fieldName, ErrTagNotProvided}
+		}
+	}
+	return ValidationError{fieldName, nil}
+}
+
+func validateInt(fieldName string, field int, tags map[string]string) ValidationError {
+	for k, v := range tags {
+		switch k {
+		case TagMax:
+			val, _ := strconv.Atoi(v)
+			if field > val {
+				return ValidationError{fieldName, ErrGraterThanMax}
+			}
+		case TagMin:
+			val, _ := strconv.Atoi(v)
+			if field < val {
+				return ValidationError{fieldName, ErrLessThanMin}
+			}
+		case TagIn:
+			if !strings.Contains(v, string(rune(field))) {
+				return ValidationError{fieldName, ErrValueOutOfRange}
+			}
+		default:
+			return ValidationError{fieldName, ErrTagNotProvided}
+		}
+	}
+	return ValidationError{fieldName, nil}
+}
+
+func validateString(fieldName string, field string, tags map[string]string) ValidationError {
+	for k, v := range tags {
+		switch k {
+		case TagRequire:
+			if field == "" {
+				return ValidationError{fieldName, ErrRequiredField}
+			}
+		case TagLength:
+			lenField, _ := strconv.Atoi(v)
+			if len([]rune(field)) != lenField {
+				return ValidationError{fieldName, ErrInvalidLength}
+			}
+		case TagRegex:
+			re, err := regexp.Compile(v)
+			if err != nil {
+				return ValidationError{fieldName, ErrInvalidRegexp}
+			}
+			if !re.MatchString(field) {
+				return ValidationError{fieldName, ErrInvalidFormat}
+			}
+		case TagIn:
+			parts := strings.Split(v, ",")
+			if !slices.Contains(parts, field) {
+				return ValidationError{fieldName, ErrValueNotInList}
+			}
+
+		default:
+			return ValidationError{fieldName, ErrTagNotProvided}
+		}
+	}
+	return ValidationError{fieldName, nil}
+}
+
+func parseTags(tagString string) map[string]string {
+	tagPairs := make(map[string]string)
+	part := tagString
+	var listTag []string
+	splitTag := strings.IndexAny(part, "|")
+	if splitTag != -1 {
+		partOne := part[:splitTag]
+		partTwo := part[splitTag+1:]
+		listTag = append(listTag, partOne)
+		listTag = append(listTag, partTwo)
+	} else {
+		listTag = append(listTag, part)
+	}
+	for _, res := range listTag {
+		keyEnd := strings.IndexAny(res, "=: ")
+		if keyEnd == -1 {
+			tagPairs[res] = ""
+			continue
+		}
+		key := res[:keyEnd]
+		val := res[keyEnd+1:]
+		if len(val) > 0 && val[0] == '"' {
+			val = strings.Trim(val, `"`)
+		}
+		tagPairs[key] = val
+	}
+	return tagPairs
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,10 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"  //nolint
+	"github.com/stretchr/testify/require" //nolint
 )
 
 type UserRole string
@@ -15,7 +17,7 @@ type (
 		Name   string
 		Age    int             `validate:"min:18|max:50"`
 		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
+		Role   UserRole        `validate:"in:admin, stuff"`
 		Phones []string        `validate:"len:11"`
 		meta   json.RawMessage //nolint:unused
 	}
@@ -38,23 +40,68 @@ type (
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
+		name        string
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			name: "User_Invalid_Email_Fails",
+			in: User{
+				ID:     "123e4567-e89b-12d3-a456-426614174000",
+				Name:   "Ivan",
+				Age:    25,
+				Email:  "invalid email",
+				Role:   "admin",
+				Phones: []string{"79991234567"},
+			},
+			expectedErr: ErrInvalidFormat,
 		},
-		// ...
-		// Place your code here.
+		{
+			name: "App_Invalid_Version_Length_Fails",
+			in: App{
+				Version: "0.0.1-1",
+			},
+			expectedErr: ErrInvalidLength,
+		},
+		{
+			name: "Token_No_Validation_Tags_Passes",
+			in: Token{
+				Header:    []byte("Authorization"),
+				Payload:   nil,
+				Signature: nil,
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "Response_Valid_Code_404_Passes",
+			in: Response{
+				Code: 405,
+				Body: "Not Found",
+			},
+			expectedErr: ErrValueOutOfRange,
+		},
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
-			t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Validate(tt.in)
 
-			// Place your code here.
-			_ = tt
+			if tt.expectedErr == nil {
+				assert.Equal(t, err.Error(), "no errors", tt.name)
+			} else {
+				require.Error(t, err, "Expected error for %s", tt.name)
+				assert.ErrorIs(t, err, tt.expectedErr, "Error mismatch for %s", tt.name)
+			}
 		})
 	}
+}
+
+func TestNoStructType(t *testing.T) {
+	user := 1
+	err := Validate(user)
+
+	t.Logf("Actual error type: %T", err)
+	t.Logf("Actual error: %+v", err)
+	require.Error(t, err, "Expected error for %s")
+	assert.ErrorIs(t, err, ErrExpectedStruct, "Error mismatch for %s")
 }
